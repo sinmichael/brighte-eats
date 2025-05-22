f4d9f59b1a12b995287975c837baf6629134093b
sinmichael <seanmichaelcvasquez@gmail.com>
2025-05-22 20:21:01 +0800

    fix: change background color

--- brighte-eats-frontend/src/App.vue
+++ brighte-eats-frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <nav class="p-4 bg-gray-100 mb-4">
+    <nav class="p-4 bg-gray-400 mb-4">
       <router-link to="/" class="mr-4">Register Lead</router-link>
       <router-link to="/leads">View Leads</router-link>
     </nav>
