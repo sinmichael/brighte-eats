f6178dcb80b0860cb298a4a7b271f2ed4be48d03
sinmichael <seanmichaelcvasquez@gmail.com>
2025-05-23 22:38:42 +0800

    fix: nav color

--- brighte-eats-frontend/src/App.vue
+++ brighte-eats-frontend/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <nav class="p-4 bg-gray-400 mb-4">
-      <router-link to="/" class="mr-4">Register Lead</router-link>
-      <router-link to="/leads">View Leads</router-link>
+    <nav class="p-4 bg-indigo-600 text-white mb-4">
+      <router-link to="/" class="mr-4 hover:bg-indigo-700 py-2 px-4 rounded-md">Register Lead</router-link>
+      <router-link to="/leads" class="hover:bg-indigo-700 py-2 px-4 rounded-md">View Leads</router-link>
     </nav>
     <router-view />
   </div>
